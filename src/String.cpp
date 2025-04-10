0c2930ec07a68543367ddb732f8f37fad6f34f5d
laurajacint0 <lyjacint@uci.edu>
2025-04-10 01:07:15 +0000

    implemented contructors, destructor and other functions

--- src/String.cpp
+++ src/String.cpp
@@ -1,6 +1,110 @@
 #include "String.h"
 #include "Alloc.h"
 
+String(const char *s = ""){
+    buf = strdup(s);
+}
+
+//constructs this string as a copy of string s
+String::String(const String & s){
+        buf = strdup(s.buf);
+    }
+
+//construct this string by moving from string s
+String::String(String && s){
+    buf = s.buf;
+    s.buf = nullptr;
+}
+
+    //assignment to this string by moving from string s
+    String &String::operator = (String &&s){
+        
+    }
+
+    //assignment operator from one string, s, to this string
+    String &String::operator = (const String & s){
+    
+    }
+
+    //allow indexing this string with notation s[i]
+    char &String::operator[] (int index){
+    
+    }
+
+    //returns the logical length of this string(# of chars up to '\0')
+    size_t String::size() const{
+        return strlen(buf);
+    }
+
+    //returns a reversal of this string, does not modify this string
+    String String::reverse(){
+
+    }
+
+    //returns index into this string for the fist occurance of c
+    int String::indexOf(const char c){
+    }
+
+    //returns index into this string for first occurance of s
+    int String::indexOf(const String s){
+
+    }
+
+    //relational operators for comparing this strings to another string
+    bool String::operator == (const String &s){
+
+    }
+
+    bool String::operator != (const String &s){
+
+    }
+    bool String::operator > (const String &s){
+
+    }
+    bool String::operator < (const String &s){
+
+    }
+    bool String::operator <= (const String &s){
+
+    }
+    bool String::operator >= (const String &s){
+
+    }
+
+    //concatenate this and s to form a return string
+    String String::operator+(const String &s){
+
+    }
+
+    //concatenate s onto the end of this string
+    String &String::operator+= (const String s){
+        *this = *this + s;
+        return *this;
+    };
+
+    //print this string, hint: use operator << to send buf to out
+    void String::print(std::ostream &out){
+        out << buf;
+    }
+
+    //read next word into this string
+    //hint: use operator >> to read from in into buf
+    void String::read(std::istream &in){
+        char temp[1024];
+        in >> temp;
+        *this = String(temp);
+    }
+
+    //destructor for a this string
+    String::~String(){
+        Alloc::delete_char_array(buf);
+    }
+
+//private: //leave public for autograder testing
+bool String::in_bounds(int i) {
+        return i >= 0 && i < this->size();
+    }
+
 size_t String::strlen(const char *s){
     size_t len = 0;
     while(s[len] != '\0'){
